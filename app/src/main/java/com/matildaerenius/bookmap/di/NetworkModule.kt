27e7d2b1f97c065189a9758d360a5ae78d0a0299
Matilda Erenius <matildaerenius@hotmail.com>
2026-03-21 17:02:08 +0100

    Implement NetworkModule with Retrofit and kotlinx.serialization

---
+++ app/src/main/java/com/matildaerenius/bookmap/di/NetworkModule.kt
@@ -0,0 +1,42 @@
+package com.matildaerenius.bookmap.di
+
+import com.matildaerenius.bookmap.data.remote.api.BookBeatApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json {
+        return Json {
+            ignoreUnknownKeys = true
+            coerceInputValues = true
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(json: Json): Retrofit {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl("https://api.bookbeat.com/")
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBookBeatApi(retrofit: Retrofit): BookBeatApi {
+        return retrofit.create(BookBeatApi::class.java)
+    }
+}
