31bbba1bcd133391c6bad6ab9f501bab4e90360e
Matilda Erenius <matildaerenius@hotmail.com>
2026-03-22 10:48:19 +0100

    Add test function in MainActivity.kt to check if fetching books work

--- app/src/main/java/com/matildaerenius/bookmap/MainActivity.kt
+++ app/src/main/java/com/matildaerenius/bookmap/MainActivity.kt
@@ -1,38 +1,57 @@
 package com.matildaerenius.bookmap
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.matildaerenius.bookmap.domain.repository.BookRepository
 import com.matildaerenius.bookmap.presentation.theme.BookmapTheme
+import dagger.hilt.android.AndroidEntryPoint
+import jakarta.inject.Inject
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var repository: BookRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        testFetchBooks()
         setContent {
             BookmapTheme {
-                val stockholm = LatLng(59.3293, 18.0686)
-                val cameraPositionState = rememberCameraPositionState {
-                    position = CameraPosition.fromLatLngZoom(stockholm, 10f)
-                }
-
-                GoogleMap(
+                Surface(
                     modifier = Modifier.fillMaxSize(),
-                    cameraPositionState = cameraPositionState
-                )
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text("Kolla Logcat för att se om det funkar")
+                }
             }
+        }
+    }
+
+    private fun testFetchBooks() {
+        lifecycleScope.launch {
+            val bookIds = listOf(1765311)
+
+            Log.d("BookTest", "Hämtar böcker...")
+            val result = repository.getBooksByIds(bookIds)
+
+            Log.d("BookTest", "Resultat: $result")
         }
     }
 }
