807d5c49582ddbce28191bedb3db49a8b58cd6aa
Matilda Erenius <matildaerenius@hotmail.com>
2026-03-23 16:55:01 +0100

    Add GetBookMarkersUseCase to combine data sources
    - Added BookMapMarker model and GetBookMarkersUseCase
    - Updated MainActivity to verify the output

---
+++ app/src/main/java/com/matildaerenius/bookmap/domain/model/BookMapMarker.kt
@@ -0,0 +1,12 @@
+package com.matildaerenius.bookmap.domain.model
+
+data class BookMapMarker(
+    val bookId: Int,
+    val locationName: String,
+    val latitude: Double,
+    val longitude: Double,
+    val description: String,
+    val bookTitle: String,
+    val bookAuthor: String,
+    val bookImageUrl: String
+)
