ca90d99f3d7e434770f67560522d55d33a33c690
Matilda Erenius <matildaerenius@hotmail.com>
2026-04-05 00:03:29 +0530

    Make MarkerDao reactive and add domain mappers
    - Updated getAllMarkers in MarkerDao to return a Flow instead of a suspend function
    - Created MarkerMapper with extension functions to convert between the BookMapMarker domain model and MarkerEntity data model.

--- app/src/main/java/com/matildaerenius/bookmap/data/local/dao/MarkerDao.kt
+++ app/src/main/java/com/matildaerenius/bookmap/data/local/dao/MarkerDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.matildaerenius.bookmap.data.local.entity.MarkerEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MarkerDao {
@@ -12,7 +13,7 @@ interface MarkerDao {
     suspend fun insertMarkers(markers: List<MarkerEntity>)
 
     @Query("SELECT * FROM marker_entity")
-    suspend fun getAllMarkers(): List<MarkerEntity>
+    fun getAllMarkers(): Flow<List<MarkerEntity>>
 
     @Query("DELETE FROM marker_entity")
     suspend fun clearMarkers()
